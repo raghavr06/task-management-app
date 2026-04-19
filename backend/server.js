7f483500331f25b4600d45cbba72c69456c0a91c
Raghav Ranjanikar <raghavranjanikar06@gmail.com>
2026-04-19 19:20:58 +0530

    Added static file serving and catch-all route
    Serve React build and handle catch-all routing.

--- backend/server.js
+++ backend/server.js
@@ -8,6 +8,7 @@ const taskRoutes = require('./routes/tasks');
 const app = express();
 const PORT = process.env.PORT || 5000;
 
+const path = require("path");
 // Middleware
 app.use(cors());
 app.use(express.json());
@@ -16,8 +17,15 @@ app.use(express.json());
 app.use('/tasks', taskRoutes);
 
 // Health check
-app.get('/', (req, res) => {
+/* app.get('/', (req, res) => {
   res.json({ message: 'Task Management API is running!' });
+}); */
+// Serve React build
+app.use(express.static(path.join(__dirname, "build")));
+
+// Catch-all route (React routing)
+app.use((req, res) => {
+  res.sendFile(path.join(__dirname, "build", "index.html"));
 });
 
 // Connect to MongoDB and start server
